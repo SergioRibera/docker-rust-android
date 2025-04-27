4413f38c027031e42377a9433935aa258dee68cb
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-04-27 01:50:55 -0400

    chore: update examples

--- examples/bevy_game/src/lib.rs
+++ examples/bevy_game/src/lib.rs
@@ -1,58 +1,88 @@
-use bevy::prelude::*;
+/// This is a Bevy game Example from
+/// https://github.com/bevyengine/bevy/blob/main/examples/animation/animated_ui.rs
+///
+use bevy::{
+    color::palettes::css::{ORANGE, SILVER, WHITE},
+    math::vec3,
+    prelude::*,
+};
+
+#[derive(Component)]
+struct Curve(CubicCurve<Vec3>);
 
 #[bevy_main]
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .add_system(update)
+        .add_systems(Startup, setup)
+        .add_systems(Update, animate_cube)
         .run();
 }
 
-#[derive(Component)]
-struct ImageAnimated;
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Define your control points
+    // These points will define the curve
+    // You can learn more about bezier curves here
+    // https://en.wikipedia.org/wiki/B%C3%A9zier_curve
+    let points = [[
+        vec3(-6., 2., 0.),
+        vec3(12., 8., 0.),
+        vec3(-12., 8., 0.),
+        vec3(6., 2., 0.),
+    ]];
 
-fn update(time: Res<Time>, mut images: Query<&mut Transform, With<ImageAnimated>>) {
-    let mut transform = images.single_mut();
-    transform.rotate_z(time.delta_seconds() * 5.);
+    // Make a CubicCurve
+    let bezier = CubicBezier::new(points).to_curve();
+
+    // Spawning a cube to experiment on
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Cuboid::default()),
+            material: materials.add(Color::from(ORANGE)),
+            transform: Transform::from_translation(points[0][0]),
+            ..default()
+        },
+        Curve(bezier),
+    ));
+
+    // Some light to see something
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            intensity: 10_000_000.,
+            range: 100.0,
+            ..default()
+        },
+        transform: Transform::from_xyz(8., 16., 8.),
+        ..default()
+    });
+
+    // ground plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Plane3d::default().mesh().size(50., 50.)),
+        material: materials.add(Color::from(SILVER)),
+        ..default()
+    });
+
+    // The camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0., 6., 12.).looking_at(Vec3::new(0., 3., 0.), Vec3::Y),
+        ..default()
+    });
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // ui camera
-    commands.spawn(Camera2dBundle::default());
-    commands
-        .spawn(NodeBundle {
-            style: Style {
-                display: Display::Flex,
-                size: Size::all(Val::Percent(100.0)),
-                align_items: AlignItems::Center,
-                flex_direction: FlexDirection::Column,
-                justify_content: JustifyContent::Center,
-                gap: Size::height(Val::Px(30.)),
-                ..default()
-            },
-            ..default()
-        })
-        .with_children(|parent| {
-            parent.spawn(TextBundle::from_section(
-                "Bevy Game",
-                TextStyle {
-                    font: asset_server.load("fonts/Lato-Regular.ttf"),
-                    font_size: 60.0,
-                    color: Color::rgb(0.9, 0.9, 0.9),
-                },
-            ));
-            parent
-                .spawn(ImageBundle {
-                    image: UiImage::new(asset_server.load("bevy_icon.png")),
-                    style: Style {
-                        min_size: Size::all(Val::Px(150.)),
-                        max_size: Size::all(Val::Px(150.)),
-                        ..Default::default()
-                    },
-                    background_color: Color::WHITE.into(),
-                    ..Default::default()
-                })
-                .insert(ImageAnimated);
-        });
+fn animate_cube(time: Res<Time>, mut query: Query<(&mut Transform, &Curve)>, mut gizmos: Gizmos) {
+    let t = (time.elapsed_seconds().sin() + 1.) / 2.;
+
+    for (mut transform, cubic_curve) in &mut query {
+        // Draw the curve
+        gizmos.linestrip(cubic_curve.0.iter_positions(50), WHITE);
+        // position takes a point from the curve where 0 is the initial point
+        // and 1 is the last point
+        transform.translation = cubic_curve.0.position(t);
+    }
 }
